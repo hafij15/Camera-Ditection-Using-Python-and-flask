a04c3609a17437b1cb584622464f34eeaf2fc2d9
Hafijur Rahman <hafij.sabuj@gmail.com>
2021-12-27 01:26:55 +0600

    index file added.

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ import cv2
 app = Flask(__name__)
 
 
-# use 0 for web camera
-camera = cv2.VideoCapture('rtsp://admin:dmabd987@192.168.0.131:554/user=username_password='password'_channel=channel_number_stream=0.sdp')
+camera = cv2.VideoCapture('rtsp://admin:dmabd987@192.168.0.131:554/user=username_password='password'_channel=channel_number_stream=0.sdp')  # use 0 for web camera
 #camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
-
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -24,8 +22,6 @@ def gen_frames():  # generate frame by frame from camera
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 #Define route
-
-
 @app.route('/video_feed')
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
